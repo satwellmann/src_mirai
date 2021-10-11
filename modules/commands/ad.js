f96eec4ab9819d0d7efbf37f802ef2b9c33355ec
Công Minh Đào <ou@example.comyou@example.com>
2021-10-11 08:09:59 +0700

    first commit

---
+++ modules/commands/ad.js
@@ -0,0 +1,11 @@
+﻿module.exports.config = {
+	name: "ad",
+	version: "1.0.0",
+	hasPermssion: 0,
+	credits: "Hà Mạc Trường Giang",
+	description: "Thông tin về admin",
+	commandCategory: "Thông tin về admin",
+	cooldowns: 0
+};
+
+module.exports.run = ({ event, api }) => api.sendMessage(`\n★Thông Tin Admin Bot Này★\nADMIN NAME : Đào Công Minh\nSinh ngày : 06/07/2005\nLink Facebook : https://www.facebook.com/daocongminh7845\nVài lời tới người dùng BOT: Vui lòng không spam khi sử dụng để tránh die bot. Cảm ơn mọi người đã sử dụng đên con bot của mình.\nLưu ý : Đừng có dại dột mà add 2 bot kẻo bị phát hiện là bạn toang đó <3\nCảnh báo : Vui lòng không dùng bot với mục đích xấu hay cố ý report acc facebook\nChúc bạn sử dụng vui vẻ <3\n=== Đào Công Minh ===`, event.threadID, event.messageID);
